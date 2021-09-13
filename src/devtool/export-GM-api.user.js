dfae4c04bc48bb96830ae27776525e0086fc4329
Cologler <skyoflw+github@gmail.com>
2021-09-13 12:15:47 +0800

    feat: add export GM api devtool

---
+++ src/devtool/export-GM-api.user.js
@@ -0,0 +1,102 @@
+// ==UserScript==
+// @name               devtool: export GM Api to global
+// @name:zh-CN         devtool: 在控制台启用 GM API
+// @namespace          https://github.com/Cologler/monkeys-javascript
+// @version            1.0
+// @description        export GM Api to global (window)
+// @author             cologler
+// @match              http://*/*
+// @match              https://*/*
+// @grant              unsafeWindow
+// @grant              GM_addStyle
+// @grant              GM_deleteValue
+// @grant              GM_listValues
+// @grant              GM_addValueChangeListener
+// @grant              GM_removeValueChangeListener
+// @grant              GM_setValue
+// @grant              GM_getValue
+// @grant              GM_log
+// @grant              GM_getResourceText
+// @grant              GM_getResourceURL
+// @grant              GM_registerMenuCommand
+// @grant              GM_unregisterMenuCommand
+// @grant              GM_openInTab
+// @grant              GM_xmlhttpRequest
+// @grant              GM_download
+// @grant              GM_getTab
+// @grant              GM_saveTab
+// @grant              GM_getTabs
+// @grant              GM_notification
+// @grant              GM_setClipboard
+// @grant              GM_info
+// @grant              GM.addStyle
+// @grant              GM.deleteValue
+// @grant              GM.listValues
+// @grant              GM.addValueChangeListener
+// @grant              GM.removeValueChangeListener
+// @grant              GM.setValue
+// @grant              GM.getValue
+// @grant              GM.log
+// @grant              GM.getResourceText
+// @grant              GM.getResourceURL
+// @grant              GM.registerMenuCommand
+// @grant              GM.unregisterMenuCommand
+// @grant              GM.openInTab
+// @grant              GM.xmlhttpRequest
+// @grant              GM.download
+// @grant              GM.getTab
+// @grant              GM.saveTab
+// @grant              GM.getTabs
+// @grant              GM.notification
+// @grant              GM.setClipboard
+// @grant              GM.info
+// @noframes
+// ==/UserScript==
+
+// just let type script work.
+(function() { function require(){}; require("greasemonkey"); })();
+
+(function() {
+    'use strict';
+
+    const GMAPI = {};
+
+    // sync api
+    GMAPI.GM_addStyle = GM_addStyle;
+    GMAPI.GM_deleteValue = GM_deleteValue;
+    GMAPI.GM_listValues = GM_listValues;
+    GMAPI.GM_addValueChangeListener = GM_addValueChangeListener;
+    GMAPI.GM_removeValueChangeListener = GM_removeValueChangeListener;
+    GMAPI.GM_setValue = GM_setValue;
+    GMAPI.GM_getValue = GM_getValue;
+    GMAPI.GM_log = GM_log;
+    GMAPI.GM_getResourceText = GM_getResourceText;
+    GMAPI.GM_getResourceURL = GM_getResourceURL;
+    GMAPI.GM_registerMenuCommand = GM_registerMenuCommand;
+    GMAPI.GM_unregisterMenuCommand = GM_unregisterMenuCommand;
+    GMAPI.GM_openInTab = GM_openInTab;
+    GMAPI.GM_xmlhttpRequest = GM_xmlhttpRequest;
+    GMAPI.GM_download = GM_download;
+    GMAPI.GM_getTab = GM_getTab;
+    GMAPI.GM_saveTab = GM_saveTab;
+    GMAPI.GM_getTabs = GM_getTabs;
+    GMAPI.GM_notification = GM_notification;
+    GMAPI.GM_setClipboard = GM_setClipboard;
+    GMAPI.GM_info = GM_info;
+
+    // async api
+    GMAPI.GM = GM;
+
+    // other
+    GMAPI.context_Window = window;
+
+    // export
+    const global = unsafeWindow;
+    for (const exportName of ['GMAPI', 'GM_API', 'GM_API_' + crypto.getRandomValues(new Uint32Array(1))[0].toString()]) {
+        if (unsafeWindow[exportName] === undefined) {
+            unsafeWindow[exportName] = GMAPI;
+            console.info(`exported GM Api (${exportName}) to global`);
+            return;
+        }
+    }
+})();
